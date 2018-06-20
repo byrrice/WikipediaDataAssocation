3b7892aa97ac29cfff420a619d3c832dfd6c950b
Isaac Ng <32532726+byrrice@users.noreply.github.com>
2018-06-20 17:02:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,44 @@ var request = require('request');
 var cheerio = require('cheerio');
 var searchTerm = 'James_Blake_(tennis)';
 var url = "https://en.wikipedia.org/w/index.php?search=" + searchTerm;
+var data;
 
-request(url, function(err, resp, body){
-    var ar1 = []
-    var ar2 = []
-    $ = cheerio.load(body);
-    links = $('p').find('a');
-    $(links).each(function(i, link){
-        ar1.push($(link).text());
-        ar2.push($(link).attr('href'));
-        // console.log($(link).text());
-        // console.log($(link).attr('href'));
-        //have to run through and delete all arrays associated with number or hashtag
+
+function getData(callback){
+    request(url, function(err, resp, body){
+        if(err){ return callback(err) };
+        if(!err){
+            var ar1 = []
+            var ar2 = []
+            $ = cheerio.load(body);
+            links = $('a');
+            $(links).each(function(i, link){
+                if ($(link).text().length > 0 && typeof($(link).attr('href')) != "undefined"){
+                    if ($(link).attr('href').startsWith('/wiki')){
+                        ar1.push($(link).text());
+                        ar2.push($(link).attr('href'));
+                    }
+                }
+            });
+            var results = [ar1, ar2];
+            callback(null, results);
+        }
     });
+}
+
+
+
+getData(function(err, results){
+    if (typeof(err) != null){
+        data = results;
+    }
+    else{
+        data = err;
+    }
+    return data;
 });
+
+
+
+
+      
